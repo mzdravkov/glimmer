93526b44484ec7a5f5ae5b6753cc5f7de6fe494f
Mihail Zdravkov <mihail0zdravkov@gmail.com>
2016-01-16 02:20:27 +0200

    Create the skeleton of the 'separate input and output channels' mechanic for the runtime

--- runtime.go
+++ runtime.go
@@ -5,59 +5,92 @@ import (
 	"sync"
 )
 
+type Channels struct {
+	Input, Output reflect.Value
+}
+
 type Glimmer struct {
-	// map with keys that are the original chans and values that are their corresponding mocks
-	MockChannels map[reflect.Value]reflect.Value
-	Mutex        *sync.Mutex
+	Channels map[reflect.Value]*Channels
+	Mutex    *sync.Mutex
 }
 
 var glimmer Glimmer
 
 func init() {
 	glimmer = Glimmer{
-		MockChannels: make(map[reflect.Value]reflect.Value),
+		Channels: make(map[reflect.Value]*Channels),
 	}
+
+	glimmer.StartTransponder()
 }
 
-func (g *Glimmer) AddChan(ch interface{}) reflect.Value {
+//TODO: find a good way to optimize this to not take all the channels from scratch every time
+func (g *Glimmer) StartTransponder() {
+	go func() {
+		for {
+			keys := make([]reflect.SelectCase, len(g.Channels))
+			for key := range g.Channels {
+				selectCase := reflect.SelectCase{Dir: reflect.SelectRecv, Chan: key}
+				keys = append(keys, selectCase)
+			}
 
-	chType := reflect.TypeOf(ch).Elem()
+			chosen, inputMessage, _ := reflect.Select(keys)
 
+			outputMessage := g.ProcessMessage(inputMessage)
+
+			g.Channels[keys[chosen].Chan].Output.Send(outputMessage)
+		}
+	}()
+}
+
+//TODO
+func (g *Glimmer) ProcessMessage(message reflect.Value) reflect.Value {
+	return message
+}
+
+//TODO shoild I make a DeleteChan method that to be invoked on delete(chan) calls?
+func (g *Glimmer) AddChan(ch interface{}) *Channels {
 	key := reflect.ValueOf(ch)
 
 	g.Mutex.Lock()
 	defer func() { g.Mutex.Unlock() }()
 
-	if mockChan, ok := glimmer.MockChannels[key]; ok {
-		return mockChan
+	if mockChannels, ok := g.Channels[key]; ok {
+		return mockChannels
 	}
 
-	mockChan := reflect.MakeChan(chType, reflect.ValueOf(ch).Cap())
+	chType := reflect.TypeOf(ch).Elem()
 
-	glimmer.MockChannels[key] = mockChan
+	inputChan := reflect.MakeChan(chType, reflect.ValueOf(ch).Cap())
+	outputChan := reflect.MakeChan(chType, reflect.ValueOf(ch).Cap())
 
-	return mockChan
+	g.Channels[key] = &Channels{Input: inputChan, Output: outputChan}
+
+	return g.Channels[key]
 }
 
+// this will be called from the modified client's code
 func (g *Glimmer) Send(ch interface{}, value interface{}) {
 	mockChan := g.AddChan(ch)
 
 	println("sending ", value)
-	mockChan.Send(reflect.ValueOf(value))
+	mockChan.Input.Send(reflect.ValueOf(value))
 }
 
+// this will be called from the modified client's code
 func (g *Glimmer) Recieve(ch interface{}) interface{} {
 	mockChan := g.AddChan(ch)
 
-	result, _ := mockChan.Recv()
+	result, _ := mockChan.Output.Recv()
 
 	return result.Interface()
 }
 
+// this will be called from the modified client's code
 func (g *Glimmer) RecieveWithBool(ch interface{}) (interface{}, bool) {
 	mockChan := g.AddChan(ch)
 
-	result, ok := mockChan.Recv()
+	result, ok := mockChan.Output.Recv()
 
 	return result.Interface(), ok
 }
